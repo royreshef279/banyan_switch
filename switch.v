b1cc96f84236afc8f2a44f57175b66d2c7842f87
Roy Reshef <64038871+royreshef279@users.noreply.github.com>
2024-03-30 22:14:23 -0400

    Update switch.v

--- switch.v
+++ switch.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/27/2024 04:46:16 PM
-// Design Name: 
-// Module Name: switch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
+//Generating 4x4 switch module by instantiating four 2x2 switches
 module switch_4_by_4(
     input [7:0] in0,in1,in2,in3,
     input [3:0] sel,
@@ -31,7 +14,7 @@ switch_2_by_2 sw4(sw1_out1,sw2_out1,out2,out3,sel[3]);
 endmodule
 
 
-
+//Generating 2x2 switch module
 module switch_2_by_2(
     input [7:0] in0,
     input [7:0] in1,
